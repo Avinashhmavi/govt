5906289edc610e11710c1a0d128b843f3ee6c368
Avinash HM <141643120+Avinashhmavi@users.noreply.github.com>
2025-04-16 17:27:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -825,4 +825,5 @@ def get_audio():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
